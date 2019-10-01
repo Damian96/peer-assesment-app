f3672155e86f3c893fd610f5211a84cd53aa7f69
Damianos Giankakis <damiang23ftr@gmail.com>
2019-10-01 21:39:50 +0300

    Adds more npm packages
    Adds extended JSPlugins to webpack.mix.config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,73 @@
 const mix = require('laravel-mix');
+const webpack = require('webpack');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+class JSPlugins {
+
+    /**
+     * All dependencies that should be installed by Mix.
+     *
+     * @return {Array}
+     */
+    // dependencies() {
+    //     return ['jquery', 'webpack-jquery-ui'];
+    // }
+
+    /**
+     * Register the component.
+     *
+     * When your component is called, all user parameters
+     * will be passed to this method.
+     *
+     * Ex: register(src, output) {}
+     * Ex: mix.yourPlugin('src/path', 'output/path');
+     *
+     * @param  {*} ...params
+     * @return {void}
+     *
+     */
+    // register() { }
+
+    /**
+     * Boot the component. This method is triggered after the
+     * user's webpack.mix.js file has executed.
+     */
+    // boot() { }
+
+    /**
+     * Override the generated webpack configuration.
+     *
+     * @param  {Object} webpackConfig
+     * @return {void}
+     */
+    // webpackConfig(webpackConfig) { }
+
+    /**
+     * Babel config to be merged with Mix's defaults.
+     *
+     * @return {Object}
+     */
+    // babelConfig() { }
+
+    /**
+     * Rules to be merged with the master webpack loaders.
+     *
+     * @return {Array|Object}
+     */
+    // webpackRules() {}
+
+    webpackPlugins() {
+        return [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery'
+            })
+        ]
+    }
+}
+
+mix.extend(JSPlugins.name.toString().toLowerCase(), new JSPlugins());
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +81,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .jsplugins();
