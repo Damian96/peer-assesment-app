35c04374206f840aaee0c9229d7cb3d86f5c72d6
Damianos Giankakis <dgiankakis@citycollege.sheffield.eu>
2020-03-29 13:07:13 +0300

    Adds custom email php and css

---
+++ resources/views/vendor/mail/html/code.blade.php
@@ -0,0 +1,8 @@
+<table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td class="code-cell">
+            <p class="code-label">{{ $label }}</p><code>{{ $slot }}</code>
+            </p>
+        </td>
+    </tr>
+</table>
