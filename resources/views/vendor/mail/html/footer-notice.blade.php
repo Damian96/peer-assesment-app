c6865ba31ea76a0714bd12ca51bd36094aea92d7
Damianos Giankakis <dgiankakis@citycollege.sheffield.eu>
2020-03-29 13:07:13 +0300

    Adds custom email php and css

---
+++ resources/views/vendor/mail/html/footer-notice.blade.php
@@ -0,0 +1,11 @@
+<table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td class="content-cell">
+            <p id="footer-notice">Please do not reply to this email. This address is for the system's automated
+                processes only. If you have any questions, please contact the <a
+                    href="mail:to{{ config('admin.mails.admin.address') }}" target="_blank"
+                    title="{{ config('admin.mails.admin.from') }}"
+                    aria-label="{{ config('admin.mails.admin.address') }}">Administration</a>.</p>
+        </td>
+    </tr>
+</table>
