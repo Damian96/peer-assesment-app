ccb83b315111e09b1b0358d8b56175870bd41ea7
damian <dgiankakis@citycollege.sheffield.eu>
2020-03-30 14:03:58 +0300

    Updates nano.sh

--- nano.sh
+++ nano.sh
@@ -1,6 +1,11 @@
 #!/bin/bash env
 
 test -r /app/ || echo "Not in heroku remote" && exit 0
+
 mkdir /app/nano
+echo -n "Created dir /app/nano"
 curl https://github.com/Ehryk/heroku-nano/raw/master/heroku-nano-2.5.1/nano.tar.gz --location --silent | tar xz -C /app/nano
+echo -n "Downloaded heroku-nano to /app/nano"
 export PATH=$PATH:/app/nano
+echo -n "Added nano editor to PATH"
+exit 0
