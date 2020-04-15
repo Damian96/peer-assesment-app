018295fef5f699d76f9a4be2827cffded25600e8
Damianos Giankakis <dgiankakis@citycollege.sheffield.eu>
2020-04-15 15:11:18 +0300

    Fixes users migration
    Signed-off-by: damian <dgiankakis@citycollege.sheffield.eu>

--- database/migrations/2019_12_03_203612_version06_2019-12-03_users.php
+++ database/migrations/2019_12_03_203612_version06_2019-12-03_users.php
@@ -30,8 +30,8 @@ class Version0620191203Users extends Migration
             $table->string('api_token', 80)->unique()->nullable()
                 ->default(null);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('last_login')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at');
+            $table->timestamp('last_login');
         });
     }
 
