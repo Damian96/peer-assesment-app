364007ec7175ac69d6c252cb6bf24380b5573bda
Damianos Giankakis <dgiankakis@citycollege.sheffield.eu>
2020-04-16 12:21:19 +0300

    Fixes nano.sh
    Signed-off-by: damian <dgiankakis@citycollege.sheffield.eu>
    
    Took 6 minutes

--- nano.sh
+++ nano.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ ! -r /app ]]; then
-   echo -n "Not in Heroku remote"
-   echo -n "Skipping nano install"
+   echo -n "Not in Heroku remote\n"
+   echo -n "Skipping nano install\n"
    exit 0
 fi
 
 mkdir /app/nano
-echo -n "Created dir /app/nano"
+echo -n "Created dir /app/nano\n"
 curl https://github.com/Ehryk/heroku-nano/raw/master/heroku-nano-2.5.1/nano.tar.gz --location --silent | tar xz -C /app/nano
-echo -n "Downloaded heroku-nano to /app/nano"
+echo -n "Downloaded heroku-nano to /app/nano\n"
 export PATH=$PATH:/app/nano
-echo -n "Added nano editor to PATH"
+echo -n "Added nano editor to PATH\n"
 exit 0
