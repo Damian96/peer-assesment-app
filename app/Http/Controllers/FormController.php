ba61b8cd6dd72e7274e8f223ab95ed76d99b03a5
Damianos <dgiankakis@citycollege.sheffield.eu>
2020-05-03 07:03:16 +0300

    Fixes FormController.php #1
    Took 7 minutes

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -428,6 +428,9 @@ class FormController extends Controller
             return redirect()->action('FormController@index', 302);
         }
 
+        if (is_string($original->questions())) {
+            $original->questions = json_decode($original->questions());
+        }
         foreach ($original->questions() as $q) {
             $model = new Question();
             $model->fill([
